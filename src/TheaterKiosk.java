121d96eee518abd02307a5f075471e8a839bd596
IMCH222 <chukwuim@mail.uc.edu>
2023-06-11 11:50:09 -0400

    Task 4 finished

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class TheaterKiosk {
     public static void main(String[] args) {
-        Scanner in = new scanner(System.in)
+        Scanner in = new Scanner(System.in);
+        int personAge = 0;
+
+        System.out.print("Enter your age: ");
+
+        personAge = in.nextInt();
+        in.nextLine();
+
+        if(personAge >= 21){
+            System.out.println("You get a wrist band!");
+        }
+
     }
 }
